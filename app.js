6ac1b0c4558b96310f2240483f0c4c078df37310
Kevin Moore <moore.kevin.p@gmail.com>
2019-12-02 15:40:32 -0500

    completed drill 2

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ app.get('/sum', (req, res) => {
   res.end(`<html><body>${sum}</body></html>`);
 });
 
+app.get('/cipher', (req, res) => {
+  const text = req.query.text;
+  const shift = Number(req.query.shift);
+  const start = 'A'.charCodeAt(0);
+  const cipher = text.toUpperCase().split('').map(letter => {
+    const code = letter.charCodeAt(0);
+    let diff = code - start;
+    diff= diff + shift;
+    const newChar = String.fromCharCode(start + diff);
+    return newChar;
+  }).join('');
+  res.set('Content-Type', 'text/html');
+  res.end(`<html><body>${cipher}</body></html>`);
+})
+
 app.listen(8000, () => {
   console.log('Express server is listening on port 8000!');
 });
